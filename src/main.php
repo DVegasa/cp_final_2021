0b7b5178cdb8c034ab7ce000bce1b4845ead02af
Ed Khalturin <DracVeg@gmail.com>
2021-12-02 18:20:00 +0300

    Класс Database

--- src/main.php
+++ src/main.php
@@ -2,29 +2,15 @@
 namespace dvegasa\cpfinal\main;
 
 use Dotenv\Dotenv;
-use PDO;
-use PDOException;
+use dvegasa\cpfinal\database\Database;
 
 function main (array $args): void {
     loadEnvVars();
-
-    try {
-        $dbHost = $_ENV['DB_HOST'];
-        $dbPort = $_ENV['DB_PORT'];
-        $dbName = $_ENV['DB_NAME'];
-        $dbUser = $_ENV['DB_USER'];
-        $dbPassword = $_ENV['DB_PASSWORD'];
-        $pdo = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName;user=$dbUser;password=$dbPassword");
-        echo "PDO connection object created";
-
-        var_dump($pdo->query('SELECT * FROM "test"')->fetchAll());
-    } catch(PDOException $e) {
-        echo $e->getMessage();
-    }
-
+    $db = new Database();
+    $db->test();
     echo 'Hello, world!';
 }
 
 function loadEnvVars() {
-    Dotenv::createImmutable('../')->load();
+    Dotenv::createImmutable(__DIR__ . '\..')->load();
 }
---
+++ src/storage/database.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace dvegasa\cpfinal\database;
+
+use PDO;
+
+class Database {
+    protected PDO $pdo;
+
+    function __construct () {
+        $dbHost = $_ENV['DB_HOST'];
+        $dbPort = $_ENV['DB_PORT'];
+        $dbName = $_ENV['DB_NAME'];
+        $dbUser = $_ENV['DB_USER'];
+        $dbPassword = $_ENV['DB_PASSWORD'];
+
+        $this->pdo = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName;user=$dbUser;password=$dbPassword");
+        echo "PDO connection object created";
+    }
+
+    function test (): void {
+        var_dump($this->pdo->query('SELECT * FROM "test"')->fetchAll());
+    }
+}
